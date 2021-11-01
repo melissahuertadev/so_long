c05dff4c5569cb77af634411df7a0c73d1f4cdc6
Meli Huerta M <melissahuerta.dev@gmail.com>
2021-11-01 04:03:23 -0500

    norm

--- includes/minigame.h
+++ includes/minigame.h
@@ -6,37 +6,36 @@
 /*   By: mhuerta <mhuerta@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 00:05:56 by mhuerta           #+#    #+#             */
-/*   Updated: 2021/11/01 02:33:46 by mhuerta          ###   ########.fr       */
+/*   Updated: 2021/11/01 04:01:23 by mhuerta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	MINIGAME_H
+#ifndef MINIGAME_H
 # define MINIGAME_H
 
 # include "../mlx/mlx.h"
 # include <stdlib.h>
 
-typedef struct		s_vector {
+typedef struct s_vector {
 	int		x;
 	int		y;
 }			t_vector;
 
-typedef struct		s_image {
+typedef struct s_image {
 	void		*reference;
 	t_vector	vector;
 	char		*pixels;
-	int		bits_per_pixel;
-	int		line_size;
-	int		endian;
+	int			bits_per_pixel;
+	int			line_size;
+	int			endian;
 }			t_image;
 
-typedef struct		s_scene {
+typedef struct s_scene {
 	void		*ref;
 	t_vector	vector;
 }			t_scene;
 
-
-typedef	struct		s_game {
+typedef struct s_game {
 	void		*mlx;
 	t_scene		scene;
 	t_image		image;
@@ -46,4 +45,3 @@ typedef	struct		s_game {
 t_scene	ft_new_scene(void *mlx, int w, int h, char *title);
 
 #endif
-
--- minigame.c
+++ minigame.c
@@ -6,39 +6,34 @@
 /*   By: mhuerta <mhuerta@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 01:05:33 by mhuerta           #+#    #+#             */
-/*   Updated: 2021/11/01 03:06:06 by melissa          ###   ########.fr       */
+/*   Updated: 2021/11/01 03:41:35 by mhuerta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minigame.h"
 
-int	ft_close()
+int	ft_close(void)
 {
 	exit(0);
 }
 
-t_scene		ft_new_scene(void *mlx, int w, int h, char *title)
+t_scene	ft_new_scene(void *mlx, int w, int h, char *title)
 {
 	t_scene	scene;
 
 	scene.ref = mlx_new_window(mlx, w, h, title);
 	scene.vector.x = w;
 	scene.vector.y = h;
-
 	mlx_hook(scene.ref, 17, 0, ft_close, 0);
-
-	return(scene);
+	return (scene);
 }
 
-int main()
+int	main(void)
 {
-       t_game	game;
-       
-       game.mlx = mlx_init();
-       game.scene = ft_new_scene(game.mlx, 600, 400, "minigame");
-       
+	t_game	game;
 
-       mlx_loop(game.mlx);
-
-       return 0;
+	game.mlx = mlx_init();
+	game.scene = ft_new_scene(game.mlx, 600, 400, "minigame");
+	mlx_loop(game.mlx);
+	return (0);
 }
